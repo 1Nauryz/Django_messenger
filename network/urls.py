bf72826bc7d33d344f0db713314d9cdb00d2bb0e
1Nauryz <nauryz.aitmurat@narxoz.kz>
2023-03-05 23:12:39 +0600

    messenger

---
+++ network/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from messenger import views
+
+urlpatterns = [
+    path("", views.index, name="home"),
+    path("about/", views.about, name="about"),
+    path("admin/", admin.site.urls)
+]
+
+handler404 = "messenger.views.page_not_found_view"
+handler403 = "messenger.views.page_forbidden"
+handler400 = "messenger.views.page_bad_request"
+# handler500 = "messenger.views.page_internal_server"
